6ecd56021083c407a7a2e0a17e278d4a9b7dbf19
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-10-06 02:00:35 -0700

    #

--- factors.c
+++ factors.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-int is_prime(unsigned long long num) {
-    if (num < 2) return 0;
-    if (num == 2 || num == 3) return 1;
-    if (num % 2 == 0 || num % 3 == 0) return 0;
-    for (unsigned long long i = 5; i * i <= num; i += 6) {
-        if (num % i == 0 || num % (i + 2) == 0) return 0;
-    }
-    return 1;
-}
-
-void rsa_factorize(unsigned long long n) {
+void factorize(unsigned long long n) {
     unsigned long long p = 1, q = n;
-
     for (p = 2; p * p <= n; p++) {
         if (n % p == 0) {
             q = n / p;
-            if (is_prime(p) && is_prime(q)) {
-                break;
-            }
+            break;
         }
     }
-
     if (p < q) {
         unsigned long long temp = p;
         p = q;
@@ -45,9 +30,9 @@ int main(int argc, char *argv[]) {
     }
 
     unsigned long long num;
-    fscanf(file, "%llu", &num);
-
-    rsa_factorize(num);
+    while (fscanf(file, "%llu", &num) != EOF) {
+        factorize(num);
+    }
 
     fclose(file);
     return 0;
